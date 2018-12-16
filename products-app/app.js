f7ca7e3ff673ed41d0a7e2e0159896a6071f99a7
Felipe Mallea <felipe@llipe.com>
2018-12-16 15:03:53 -0300

    Docker-composed with node + mongo with a wait scrit to sync the starting process

--- products-app/app.js
+++ products-app/app.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 const product = require('./routes/product.route');
 
 // Set up mongoose connection
-let dev_db_url = 'mongodb://productUser:productPassword@localhost:27017/productdb';
+let dev_db_url = 'mongodb://localhost:27017/productdb';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
-let port = process.env.HTTP_PORT || 7000;
+let port = process.env.HTTP_PORT || 1234;
 app.listen(port, () => {
     console.log('Server running on port ' + port);
 });
